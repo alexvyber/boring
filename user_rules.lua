380b18fe347fa9835129d51d0850011b49956e37
Alexey Sokolov <alexvyber@gmail.com>
2022-11-05 09:06:08 +0300

    reg

--- user_rules.lua
+++ user_rules.lua
@@ -5,6 +5,7 @@ return {
 	vivaldi = { rule = { instance = "vivaldi" }, properties = { tag = "5" } },
 	chrome = { rule = { instance = "google-chrome" }, properties = { tag = "17" } },
 	obs = { rule = { instance = "obs" }, properties = { tag = "8" } },
+	chromium = { rule = { instance = "chromium-browser" }, properties = { tag = "3" } },
 	chrome_beta = { rule = { instance = "google-chrome-beta" }, properties = { tag = "10" } },
 	telegram = { rule = { instance = "telegram" }, properties = { tag = "12" } },
 	qbittorrent = { rule = { instance = "qbittorrent" }, properties = { tag = "13" } },
@@ -12,7 +13,7 @@ return {
 	vlc = { rule = { instance = "vlc" }, properties = { tag = "15" } },
 
 
-	mullvad = { rule = { instance = "mullvad" }, properties = { tag = "6" } },
+	mullvad = { rule = { instance = "mullvad" }, properties = { tag = "r" } },
 	--[[ blue = { rule = { instance = "blueman-manager" }, properties = { tag = "6" } }, ]]
 	--[[ slack = { rule = { instance = "slack" }, properties = { tag = "10" } }, ]]
 	--[[ discord = { rule = { instance = "discord" }, properties = { tag = "11" } }, ]]
